1be5155a7070cb3c56791f5cc5c40a0f957fd9f4
YangJun <261058142@qq.com>
2019-04-03 13:42:30 +0800

    Initial commit

---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.atguigu.java;
+
+/**
+ * @author Alex
+ * @create 2019-04-03 10:34
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
